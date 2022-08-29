f03320ddec855d3e70c314b621d5734428d867a9
Laszlo Deak <ladeak@windowslive.com>
2022-08-29 23:08:51 +0200

    SR resources

---
+++ src/Http3Repl/SR.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace System.Net.Http
+{
+    internal partial class SR
+    {
+        internal static string Format(string resourceFormat, params object[] args)
+        {
+            if (args != null)
+            {
+                return string.Format(CultureInfo.CurrentCulture, resourceFormat, args);
+            }
+
+            return resourceFormat;
+        }
+    }
+}
