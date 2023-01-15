15d3c88940426db3467d53d671c80343f6c85ba6
Laszlo Deak <ladeak87@windowslive.com>
2023-01-15 17:31:45 +0100

    Sample test app

--- tests/TestWebApplication/Program.cs
+++ tests/TestWebApplication/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,10 @@ builder.WebHost.UseKestrel(kestrel =>
     kestrel.ListenAnyIP(5001, options =>
     {
         options.UseHttps();
-        options.Protocols = HttpProtocols.Http3;
+        options.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
     });
 });
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 app.MapGet("/", async context =>
@@ -16,4 +18,27 @@ app.MapGet("/", async context =>
     await context.Response.WriteAsync("Hello World " + context.Request.Protocol.ToString());
 });
 
+app.MapGet("/long", async (HttpContext context, [FromServices] HttpClient client) =>
+{
+    var stream = await client.GetStreamAsync("https://www.gutenberg.org/files/1661/1661-0.txt");
+    await stream.CopyToAsync(context.Response.Body);
+});
+
+app.MapGet("/longer", async (HttpContext context, [FromServices] HttpClient client) =>
+{
+    var stream = await client.GetStreamAsync("https://www.gutenberg.org/cache/epub/100/pg100.txt"); 
+    await stream.CopyToAsync(context.Response.Body);
+});
+
+app.MapGet("/stream", GenerateData);
+
 app.Run();
+
+async IAsyncEnumerable<string> GenerateData()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        await Task.Delay(1000);
+        yield return $"hello {i}";
+    }
+}
