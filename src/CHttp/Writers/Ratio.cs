1cc6f438cee96d59679586898892bc2b80de1885
ladeak <ladeak87@windowslive.com>
2023-06-07 21:39:40 +0200

    Running counter in perf mode

--- src/CHttp/Writers/Ratio.cs
+++ src/CHttp/Writers/Ratio.cs
@@ -1,5 +1,32 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 
 namespace CHttp.Writers;
 
-public record struct Ratio<T>(T Numerator, T Total, TimeSpan Remaining) where T : IBinaryNumber<T>;
+public record struct Ratio<T> where T : IBinaryNumber<T>
+{
+    private long _createdTimestamp;
+    private TimeSpan _remaining;
+
+    public Ratio(T numerator, T total, TimeSpan remaining)
+    {
+        Numerator = numerator;
+        Total = total;
+        _remaining = remaining;
+        _createdTimestamp = Stopwatch.GetTimestamp();
+    }
+
+    public T Numerator { get; }
+
+    public T Total { get; }
+
+    public TimeSpan Remaining
+    {
+        get
+        {
+            var value = _remaining - TimeSpan.FromTicks(Stopwatch.GetTimestamp() - _createdTimestamp); 
+            return value > TimeSpan.Zero ? value : TimeSpan.Zero;
+        }
+    }
+}
+
