7d0bec6d9ca5a614cf69c16d70055ef7cd3be013
ladeak <ladeak87@windowslive.com>
2025-09-10 22:17:35 +0200

    VariableLenghtIntegerDecoder - updated implementation

--- src/CHttpServer/CHttpServer/Http3/VariableLenghtIntegerDecoder.cs
+++ src/CHttpServer/CHttpServer/Http3/VariableLenghtIntegerDecoder.cs
@@ -6,32 +6,33 @@ public class VariableLenghtIntegerDecoder
 {
     public static bool TryRead(ReadOnlySpan<byte> buffer, out ulong value, out int bytesRead)
     {
-        byte firstByte = buffer[0];
-        var length = firstByte & 0b11000000;
-        switch(length)
+        if (buffer.IsEmpty)
         {
-            case 0b00000000:
-                bytesRead = 1;
+            value = 0;
+            bytesRead = 0;
+            return false;
+        }
+        byte firstByte = buffer[0];
+        bytesRead = 1 << (firstByte >> 6);
+        switch (bytesRead)
+        {
+            case 1:
                 value = (ulong)(firstByte & 0x3F);
                 return true;
-            case 0b01000000:
-                bytesRead = 2;
+            case 2:
                 var result = BinaryPrimitives.TryReadUInt16BigEndian(buffer, out var parsedShortValue);
                 value = (ulong)parsedShortValue & 0x3FFF;
                 return result;
-            case 0b10000000:
-                bytesRead = 4;
+            case 4:
                 result = BinaryPrimitives.TryReadUInt32BigEndian(buffer, out var parsedIntValue);
                 value = parsedIntValue & 0x3FFFFFFF;
                 return result;
-            case 0b11000000:
-                bytesRead = 8;
+            case 8:
                 result = BinaryPrimitives.TryReadUInt64BigEndian(buffer, out value);
                 value = value & 0x3FFFFFFF_FFFFFFFF;
                 return result;
         }
         value = 0;
-        bytesRead = 0;
         return false;
     }
 }
