a769b99a510c42a62e6b31e7285f81c6d0ea11e7
ladeak <ladeak87@windowslive.com>
2026-01-15 21:46:37 +0100

    SIMD encoding up until 62 bits (long encoding)

--- src/CHttpServer/CHttpServer/Http3/QPackIntegerEncoder.cs
+++ src/CHttpServer/CHttpServer/Http3/QPackIntegerEncoder.cs
@@ -8,7 +8,7 @@ namespace CHttpServer.Http3;
 
 internal struct QPackIntegerEncoder
 {
-    public bool TryEncode(Span<byte> destination, int number, byte prefixLength, out int writtenLength)
+    public bool TryEncode(Span<byte> destination, long number, byte prefixLength, out int writtenLength)
     {
         //if I < 2 ^ N - 1, encode I on N bits
         //else
@@ -54,6 +54,7 @@ internal struct QPackIntegerEncoder
 
     }
 
+
     private static Vector256<uint> Divisor = Vector256.Create([0u, 7u, 14u, 21u, 28u, 32u, 32u, 32u]);
     private static Vector256<int> Add128 = Vector256.Create(128);
 
@@ -97,6 +98,58 @@ internal struct QPackIntegerEncoder
         var vLength = Avx2.CompareGreaterThan(Add128, vNumber).AsByte();
         writtenCount = 2 + (int.TrailingZeroCount(Avx2.MoveMask(vLength)) >> 2);
         destination[writtenCount - 1] -= 128;
+        return true;
+    }
+
+    private static Vector256<uint> DivisorLong = Vector256.Create([0u, 7u, 14u, 21u, 0u, 7u, 14u, 21u]);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool TryEncodeSimd(Span<byte> destination, long number, byte prefixLength, out int writtenCount)
+    {
+        //if I < 2 ^ N - 1, encode I on N bits
+        //else
+        //    encode(2 ^ N - 1) on N bits
+        //         I = I - (2 ^ N - 1)
+        //    while I >= 128
+        //         encode(I % 128 + 128) on 8 bits
+        //         I = I / 128
+        //    encode I on 8 bits
+        Debug.Assert(1 <= prefixLength && prefixLength <= 8);
+        Debug.Assert(number >= 0);
+
+        if (!Avx2.IsSupported)
+            return TryEncode(destination, number, prefixLength, out writtenCount);
+
+        writtenCount = 1;
+        int prefixLimit = (1 << prefixLength) - 1;
+        if (destination.Length < Vector128<byte>.Count + 2)
+            return false;
+
+        if (number < prefixLimit)
+        {
+            destination[0] = (byte)(destination[0] | number);
+            return true;
+        }
+
+        destination[0] = (byte)(destination[0] | prefixLimit);
+        number -= prefixLimit;
+
+        int upperLane = (int)(number >> 28);
+        int lowerLane = (int)(number & int.MaxValue);
+        var vNumber = Avx2.ShiftRightLogicalVariable(Vector256.Create(Vector128.Create(lowerLane), Vector128.Create(upperLane)), DivisorLong);
+        var quotient128 = Avx2.ShiftRightLogical(vNumber, 7); // Div
+        var vRemainder = Avx2.Or(vNumber - Avx2.ShiftLeftLogical(quotient128, 7), Add128);
+
+        Vector128<short> packedShort = Avx2.PackSignedSaturate(vRemainder.GetLower(), vRemainder.GetUpper());
+        Vector128<byte> narrowBytes = Avx2.PackUnsignedSaturate(packedShort, packedShort);
+        narrowBytes.StoreUnsafe(ref destination[1]);
+
+        var vLength = Avx2.CompareGreaterThan(vNumber, Vector256<int>.Zero).AsByte();
+        writtenCount = 9 - (int.LeadingZeroCount(Avx2.MoveMask(vLength)) >> 2);
+        if (writtenCount == 9)
+            destination[writtenCount++] = (byte)(number >> 56);
+        else
+            destination[writtenCount - 1] -= 128;
         return true;
     }
 }
--- tests/CHttpServer.Tests/Http3/QPackIntegerEncoderTests.cs
+++ tests/CHttpServer.Tests/Http3/QPackIntegerEncoderTests.cs
@@ -165,4 +165,100 @@ public class QPackIntegerEncoderTests
             }
         }
     }
+
+    [Fact]
+    public void EncodeSimd1337_Long()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        var sut = new QPackIntegerEncoder();
+        sut.TryEncodeSimd(buffer, 1337L, 5, out var length);
+        byte[] b = [0b00011111, 0b10011010, 0b00001010];
+        Assert.True(b.SequenceEqual(buffer[..length]));
+    }
+
+
+    [Fact]
+    public void EncodeSimd2_Long()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        var sut = new QPackIntegerEncoder();
+        Assert.True(sut.TryEncodeSimd(buffer, 2L, 1, out var length));
+        byte[] b = [0b00000001, 0b00000001];
+        Assert.True(b.SequenceEqual(buffer[..length]));
+    }
+
+    [Fact]
+    public void EncodeSimd167225_Long()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        var sut = new QPackIntegerEncoder();
+        Assert.True(sut.TryEncodeSimd(buffer, 167225L, 5, out var length));
+        byte[] b = [0b00011111, 0b10011010, 0b10011010, 0b00001010];
+        Assert.True(b.SequenceEqual(buffer[..length]));
+    }
+
+    [Fact]
+    public void EncodeSimd0_Long()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        var sut = new QPackIntegerEncoder();
+        Assert.True(sut.TryEncodeSimd(buffer, 0L, 5, out var length));
+        byte[] b = [0b00000000];
+        Assert.True(b.SequenceEqual(buffer[..length]));
+    }
+
+    [Fact]
+    public void EncodeSimd2147483647_7_Long()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        var sut = new QPackIntegerEncoder();
+        Assert.True(sut.TryEncodeSimd(buffer, 2147483647, 7, out var length));
+        byte[] b = [0b0111_1111, 0b10000000, 0b11111111, 0b11111111, 0b11111111, 0b00000111];
+        Assert.True(b.SequenceEqual(buffer[..length]));
+    }
+
+    [Fact]
+    public void EncodeSimd2147483647_Prefix1_Long()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        var sut = new QPackIntegerEncoder();
+        Assert.True(sut.TryEncodeSimd(buffer, 2147483647L, 1, out var length));
+        byte[] b = [0b0000_0001, 0b11111110, 0b11111111, 0b11111111, 0b11111111, 0b00000111];
+        Assert.True(b.SequenceEqual(buffer[..length]));
+    }
+
+    [Fact]
+    public void Encode_Decode_SimdLong()
+    {
+        Span<byte> buffer = stackalloc byte[128];
+        for (long i = 1; i < uint.MaxValue; i += 127)
+        {
+            buffer.Clear();
+            var encoder = new QPackIntegerEncoder();
+            Assert.True(encoder.TryEncodeSimd(buffer, i, 5, out var length));
+            var decoder = new QPackIntegerDecoder();
+            if (decoder.BeginTryDecode(buffer[0], 5, out var result))
+                Assert.Equal(i, result);
+            else
+            {
+                int offset = 1;
+                Assert.True(decoder.TryDecode62Bits(buffer, ref offset, out long longResult));
+                Assert.Equal(i, longResult);
+            }
+        }
+    }
+
+    [Fact]
+    public void Encode_Decode_LongMax()
+    {
+        long input = long.MaxValue >> 1;
+        Span<byte> buffer = stackalloc byte[128];
+        var encoder = new QPackIntegerEncoder();
+        Assert.True(encoder.TryEncodeSimd(buffer, input, 1, out var length));
+        var decoder = new QPackIntegerDecoder();
+        decoder.BeginTryDecode(buffer[0], 1, out var _);
+        int offset = 1;
+        Assert.True(decoder.TryDecode62Bits(buffer, ref offset, out long result));
+        Assert.Equal(input, result);
+    }
 }
