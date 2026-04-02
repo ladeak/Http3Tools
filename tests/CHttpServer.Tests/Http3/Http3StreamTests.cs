1a2aa21f67b97465dce78a81c18cd5be20bf7848
ladeak <ladeak87@windowslive.com>
2026-04-02 23:10:16 +0200

    More Http3Stream tests

--- tests/CHttpServer.Tests/Http3/Http3StreamTests.cs
+++ tests/CHttpServer.Tests/Http3/Http3StreamTests.cs
@@ -11,7 +11,7 @@ public class Http3StreamTests
     private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(10);
 
     [Fact]
-    public async Task PseudoHeadersProcessed()
+    public async Task SingleWrite_HeaderFrame()
     {
         var quicConnection = await QuicConnectionFixture.SetupConnectionAsync(Port, TestContext.Current.CancellationToken);
         var serverStreamTask = Task.Run(async () => await quicConnection.ServerConnection.AcceptInboundStreamAsync());
@@ -22,7 +22,7 @@ public class Http3StreamTests
 
         TaskCompletionSource tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
         sut.Initialize(null, await serverStreamTask);
-        var testApp = new TestBase.TestApplication(ctx => 
+        var testApp = new TestBase.TestApplication(ctx =>
         {
             Assert.Equal("/", ctx.Request.Path);
             Assert.Equal("https", ctx.Request.Scheme);
@@ -36,9 +36,149 @@ public class Http3StreamTests
         await tcs.Task.WaitAsync(DefaultTimeout, TestContext.Current.CancellationToken);
         clientStream.Close();
         await processing;
+        await quicConnection.DisposeAsync();
     }
 
-    private static async Task WriteHeaders(QuicStream clientControlStream)
+    [Fact]
+    public async Task MultipleWrites_HeaderFrame()
+    {
+        var headersFrame = (await GetHeadersFrame()).AsMemory();
+        for (int i = 1; i < headersFrame.Length; i++)
+        {
+            var quicConnection = await QuicConnectionFixture.SetupConnectionAsync(Port, TestContext.Current.CancellationToken);
+            var serverStreamTask = Task.Run(async () => await quicConnection.ServerConnection.AcceptInboundStreamAsync());
+            var sut = new Http3Stream([]);
+            var clientStream = await quicConnection.ClientConnection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional, TestContext.Current.CancellationToken);
+
+            // First Write
+            await clientStream.WriteAsync(headersFrame[..i], TestContext.Current.CancellationToken);
+            await clientStream.FlushAsync(TestContext.Current.CancellationToken);
+
+            TaskCompletionSource tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+            sut.Initialize(null, await serverStreamTask);
+            var testApp = new TestBase.TestApplication(ctx =>
+            {
+                Assert.Equal("/", ctx.Request.Path);
+                Assert.Equal("https", ctx.Request.Scheme);
+                Assert.Equal("localhost", ctx.Request.Host.ToString());
+                Assert.Equal(HttpMethod.Get.ToString(), ctx.Request.Method);
+                tcs.SetResult();
+                return Task.CompletedTask;
+            });
+            var processing = sut.ProcessStreamAsync(testApp, TestContext.Current.CancellationToken);
+
+            // Second Write
+            await clientStream.WriteAsync(headersFrame[i..], TestContext.Current.CancellationToken);
+            await clientStream.FlushAsync(TestContext.Current.CancellationToken);
+
+            await tcs.Task.WaitAsync(DefaultTimeout, TestContext.Current.CancellationToken);
+            clientStream.Close();
+            await processing;
+            await quicConnection.DisposeAsync();
+        }
+    }
+
+    [Fact]
+    public async Task SingleWrite_HeaderFrame_ReservedFrame()
+    {
+        var quicConnection = await QuicConnectionFixture.SetupConnectionAsync(Port, TestContext.Current.CancellationToken);
+        var serverStreamTask = Task.Run(async () => await quicConnection.ServerConnection.AcceptInboundStreamAsync());
+        var sut = new Http3Stream([]);
+
+        var clientStream = await quicConnection.ClientConnection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional, TestContext.Current.CancellationToken);
+        byte[] data = [.. await GetHeadersFrame(), .. GetReservedFrame(10000)];
+        await clientStream.WriteAsync(data, TestContext.Current.CancellationToken);
+        await clientStream.FlushAsync(TestContext.Current.CancellationToken);
+
+        TaskCompletionSource tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+        sut.Initialize(null, await serverStreamTask);
+        var testApp = new TestBase.TestApplication(ctx =>
+        {
+            Assert.Equal("/", ctx.Request.Path);
+            Assert.Equal("https", ctx.Request.Scheme);
+            Assert.Equal("localhost", ctx.Request.Host.ToString());
+            Assert.Equal(HttpMethod.Get.ToString(), ctx.Request.Method);
+            tcs.SetResult();
+            return Task.CompletedTask;
+        });
+        var processing = sut.ProcessStreamAsync(testApp, TestContext.Current.CancellationToken);
+
+        await tcs.Task.WaitAsync(DefaultTimeout, TestContext.Current.CancellationToken);
+        clientStream.Close();
+        await processing;
+        await quicConnection.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task MultipleWrites_HeaderFrame_ReservedFrame()
+    {
+        byte[] data = [.. await GetHeadersFrame(), .. GetReservedFrame(10)];
+        for (int i = 1; i < data.Length; i++)
+        {
+            var quicConnection = await QuicConnectionFixture.SetupConnectionAsync(Port, TestContext.Current.CancellationToken);
+            var serverStreamTask = Task.Run(async () => await quicConnection.ServerConnection.AcceptInboundStreamAsync());
+            var sut = new Http3Stream([]);
+
+            var clientStream = await quicConnection.ClientConnection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional, TestContext.Current.CancellationToken);
+            await clientStream.WriteAsync(data.AsMemory(0, i), TestContext.Current.CancellationToken);
+            await clientStream.FlushAsync(TestContext.Current.CancellationToken);
+
+            TaskCompletionSource tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+            sut.Initialize(null, await serverStreamTask);
+            var testApp = new TestBase.TestApplication(ctx =>
+            {
+                Assert.Equal("/", ctx.Request.Path);
+                Assert.Equal("https", ctx.Request.Scheme);
+                Assert.Equal("localhost", ctx.Request.Host.ToString());
+                Assert.Equal(HttpMethod.Get.ToString(), ctx.Request.Method);
+                tcs.SetResult();
+                return Task.CompletedTask;
+            });
+            var processing = sut.ProcessStreamAsync(testApp, TestContext.Current.CancellationToken);
+
+            await clientStream.WriteAsync(data.AsMemory(i), TestContext.Current.CancellationToken);
+            await clientStream.FlushAsync(TestContext.Current.CancellationToken);
+
+            await tcs.Task.WaitAsync(DefaultTimeout, TestContext.Current.CancellationToken);
+            clientStream.Close();
+            await processing;
+            await quicConnection.DisposeAsync();
+        }
+    }
+
+    [Fact]
+    public async Task SingleWrite_HeaderFrame_ReservedFrame_DataFrame()
+    {
+        var quicConnection = await QuicConnectionFixture.SetupConnectionAsync(Port, TestContext.Current.CancellationToken);
+        var serverStreamTask = Task.Run(async () => await quicConnection.ServerConnection.AcceptInboundStreamAsync());
+        var sut = new Http3Stream([]);
+
+        var clientStream = await quicConnection.ClientConnection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional, TestContext.Current.CancellationToken);
+        byte[] data = [.. await GetHeadersFrame(), .. GetData(30), .. GetReservedFrame(750), .. GetData(30)];
+        await clientStream.WriteAsync(data, TestContext.Current.CancellationToken);
+        await clientStream.FlushAsync(TestContext.Current.CancellationToken);
+
+        TaskCompletionSource tcs = new(TaskCreationOptions.RunContinuationsAsynchronously);
+        sut.Initialize(null, await serverStreamTask);
+        var testApp = new TestBase.TestApplication(ctx =>
+        {
+            Assert.Equal("/", ctx.Request.Path);
+            Assert.Equal("https", ctx.Request.Scheme);
+            Assert.Equal("localhost", ctx.Request.Host.ToString());
+            Assert.Equal(HttpMethod.Get.ToString(), ctx.Request.Method);
+            // TODO: assert data
+            tcs.SetResult();
+            return Task.CompletedTask;
+        });
+        var processing = sut.ProcessStreamAsync(testApp, TestContext.Current.CancellationToken);
+
+        await tcs.Task.WaitAsync(DefaultTimeout, TestContext.Current.CancellationToken);
+        clientStream.Close();
+        await processing;
+        await quicConnection.DisposeAsync();
+    }
+
+    private static async Task<byte[]> GetHeadersFrame()
     {
         var encoder = new QPackDecoder();
         var headers = new Http3ResponseHeaderCollection
@@ -54,18 +194,28 @@ public class Http3StreamTests
         await writer.FlushAsync();
         var payloadLength = VariableLenghtIntegerDecoder.Write(ms.Length);
 
-        byte[] data = [1, .. payloadLength.Span, .. ms.GetBuffer().AsSpan(0, (int)ms.Length)];
-        await clientControlStream.WriteAsync(data, TestContext.Current.CancellationToken);
-        await clientControlStream.FlushAsync(TestContext.Current.CancellationToken);
+        return [1, .. payloadLength.Span, .. ms.GetBuffer().AsSpan(0, (int)ms.Length)];
     }
 
-    private static async Task WriteData(QuicStream clientControlStream)
+    private static async Task WriteHeaders(QuicStream clientStream)
     {
-        // TODO
+        byte[] data = await GetHeadersFrame();
+        await clientStream.WriteAsync(data, TestContext.Current.CancellationToken);
+        await clientStream.FlushAsync(TestContext.Current.CancellationToken);
     }
 
-    private static async Task WriteReservedFrame(QuicStream clientControlStream)
+    private static byte[] GetData(int length)
     {
-        // TODO
+        var payloadLength = VariableLenghtIntegerDecoder.Write(length);
+        var payload = Enumerable.Sequence(0, length - 1, 1).Select(x => (byte)x);
+        return [0, .. payloadLength.Span, .. payload];
+    }
+
+    private static byte[] GetReservedFrame(int length)
+    {
+        var frameType = VariableLenghtIntegerDecoder.Write(2 * 0x1f + 0x21);
+        var payloadLength = VariableLenghtIntegerDecoder.Write(length);
+        var payload = Enumerable.Sequence(0, length - 1, 1).Select(x => (byte)x);
+        return [.. frameType.Span, .. payloadLength.Span, .. payload];
     }
 }
