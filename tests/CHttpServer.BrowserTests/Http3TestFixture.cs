218833f21e8368a76e80ede27fc332d6e3e4aa6b
ladeak <ladeak87@windowslive.com>
2026-07-19 11:24:26 +0200

    H3 headless

--- tests/CHttpServer.BrowserTests/Http3TestFixture.cs
+++ tests/CHttpServer.BrowserTests/Http3TestFixture.cs
@@ -23,7 +23,7 @@ public sealed class Http3TestFixture : IAsyncDisposable
         PlaywrightHost = Playwright.CreateAsync().GetAwaiter().GetResult();
         Browser = PlaywrightHost.Chromium.LaunchPersistentContextAsync("/tmp/chrome-profile-integrationtest", new()
         {
-            Headless = false,
+            Headless = true,
             Args = [$"--origin-to-force-quic-on=127.0.0.1:{Port}", "--ignore-certificate-errors-spki-list=5QveYGg8xaCnnZWvkC9Y6v9lQVmF2BCozvds6Cn6F6k="]
         }).GetAwaiter().GetResult();
     }
